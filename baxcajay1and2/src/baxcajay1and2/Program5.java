5487f5b1be486494f93604db9c8e554887bbf770
zuriel <zuriel@localhost>
2020-10-17 22:30:58 -0400

    12 / 5

--- baxcajay1and2/src/baxcajay1and2/Program5.java
+++ baxcajay1and2/src/baxcajay1and2/Program5.java
@@ -18,6 +18,9 @@ public class Program5 {
 		int secondNumber = inputNumberTwo.nextInt();
 		
 		// Computes the quotient
+		// Because the number are integers no decimal point will appear on the result.
+		// For example when you divide 12 by 5 its normal 2.4 because the result will
+		// output 2 because its an integer.
 		int quotient = firstNumber / secondNumber;
 		
 		// Displays the results 
